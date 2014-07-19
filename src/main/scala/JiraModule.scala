1ae313ff526357e134d40312717ab09e223a48c6
Felix Geller <fgeller@gmail.com>
2014-07-19 14:15:44 +1200

    Create actor using server's actor system.

--- src/main/scala/JiraModule.scala
+++ src/main/scala/JiraModule.scala
@@ -1,9 +1,23 @@
 package io.yard.module.jira
 
 import akka.actor.Props
+import akka.actor.ActorSystem
+import play.api.libs.concurrent.Akka
 
 import io.yard.common.models.Module
 
 object JiraModule {
-  lazy val value = Module("jira", "JIRA", Some(JiraController), Some(Props[Jiractor]))
+
+  lazy val value: Module = {
+    import play.api.Play.current
+    val name = "jira"
+    val actor = Akka.system.actorOf(Props[Jiractor], name)
+    Module(
+      name,
+      description = "The JIRA module.",
+      controller = Some(JiraController),
+      actor = Option(actor)
+    )
+  }
+
 }
